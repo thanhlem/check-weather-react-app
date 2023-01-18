2049015f84e70708a3288ce845ec6080a1f46285
thanhlem <thanhlm.rsc@gmail.com>
2023-01-18 15:03:24 +0700

    add files from local prj

---
+++ src/WeatherInfo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import FormattedDate from "./FormattedDate";
+import "bootstrap/dist/css/bootstrap.css";
+import WeatherTemperature from "./WeatherTemperature";
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div className="float-left mt-3">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6 mt-3">
+          <ul>
+            <li className="humidity">Humidity: {props.data.humidity}%</li>
+            <li className="wind">Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
